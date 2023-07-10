272bfb03f51ebcd3896ceb2953b6353fac47ac6b
Isidor <isschoch@student.ethz.ch>
2023-07-09 21:52:05 -0400

    now with ipe version of qbc

--- qbc_conv_reduced.py
+++ qbc_conv_reduced.py
@@ -8,16 +8,14 @@ from scipy import linalg as la
 def qbc_conv(x, y, k, num_t_wires=10):
     assert len(x) == len(y)
     num_n_wires = int(np.ceil(np.log2(len(x))))
-    num_c_wires = num_n_wires
 
     t_wires = range(0, num_t_wires)
-    c_wires = range(num_t_wires, num_t_wires + num_n_wires)
-    n_wires = range(num_t_wires + num_n_wires, num_t_wires + num_n_wires + num_c_wires)
+    n_wires = range(num_t_wires, num_t_wires + num_n_wires)
     o_wires = range(
-        num_t_wires + num_n_wires + num_c_wires,
-        num_t_wires + num_n_wires + num_c_wires + 2,
+        num_t_wires + num_n_wires,
+        num_t_wires + num_n_wires + 2,
     )
-    tot_wires = range(0, num_t_wires + num_n_wires + num_c_wires + 2)
+    tot_wires = range(0, num_t_wires + num_n_wires + 2)
     num_tot_wires = len(tot_wires)
 
     dev = qml.device("default.qubit", wires=tot_wires)
@@ -42,42 +40,16 @@ def qbc_conv(x, y, k, num_t_wires=10):
     # Oracle B encoding y data
     def U_y():
         for one_idx in y_indices:
-            qml.ctrl(qml.PauliX, control=c_wires, control_values=one_idx)(
-                wires=o_wires[1]
-            )
-
-    def U_y_alt():
-        for one_idx in y_indices:
             qml.ctrl(qml.PauliX, control=n_wires, control_values=one_idx)(
                 wires=o_wires[1]
             )
-
-    def qft_adder():
-        qml.adjoint(qml.QFT(wires=c_wires))  # step 3
-        for n_idx, n_wire in enumerate(n_wires):
-            qml.ctrl(add_k_fourier, control=n_wire)(
-                2 ** (num_n_wires - n_idx - 1), wires=c_wires
-            )
-        qml.QFT(wires=c_wires)  # step 1
 
     def add_k_fourier(l, wires):
         for j in range(len(wires)):
             qml.RZ(l * np.pi / (2**j), wires=wires[j])
 
-    # Copies entries from n register to c register
+    # Subctracts indices
     def U_minus():
-        for i in range(num_n_wires):
-            qml.CNOT(wires=[n_wires[i], c_wires[i]])
-        for c_wire in c_wires:
-            qml.PauliX(wires=c_wire)
-        qml.QFT(wires=c_wires)
-        add_k_fourier(k + 1, wires=c_wires)
-        qml.adjoint(qml.QFT(wires=c_wires))
-
-    # Copies entries from n register to c register
-    def U_minus_alt():
-        # for i in range(num_n_wires):
-        #     qml.CNOT(wires=[n_wires[i], c_wires[i]])
         for n_wire in n_wires:
             qml.PauliX(wires=n_wire)
         qml.QFT(wires=n_wires)
@@ -87,16 +59,13 @@ def qbc_conv(x, y, k, num_t_wires=10):
     # Phase oracle used in Grover operator
     def phase_oracle():
         U_x()
-        # U_minus()
-        U_minus_alt()
-        U_y_alt()
-        qml.adjoint(U_minus_alt)()
-        # U_y()
+        U_minus()
+        U_y()
+        qml.adjoint(U_minus)()
         qml.CZ(wires=[o_wires[0], o_wires[1]])
-        U_minus_alt()
-        qml.adjoint(U_y_alt)()
-        qml.adjoint(U_minus_alt)()
-        # qml.adjoint(U_minus)()
+        U_minus()
+        qml.adjoint(U_y)()
+        qml.adjoint(U_minus)()
         qml.adjoint(U_x)()
 
     # Grover operator used in QPE
@@ -121,7 +90,7 @@ def qbc_conv(x, y, k, num_t_wires=10):
         my_unitary = qml.matrix(grover_operator)()
         qml.QuantumPhaseEstimation(
             my_unitary,
-            target_wires=range(num_t_wires + num_c_wires, num_tot_wires),
+            target_wires=range(num_t_wires, num_tot_wires),
             estimation_wires=t_wires,
         )
 
@@ -136,20 +105,20 @@ def qbc_conv(x, y, k, num_t_wires=10):
     return rho, f, theta, j, probs
 
 
-x = np.random.randint(0, 2, size=(16))
-y = np.random.randint(0, 2, size=(16))
+x = np.random.randint(0, 2, size=(64))
+y = np.random.randint(0, 2, size=(64))
 
 
-print("x =", x.reshape(4, 4))
-print("y =", y.reshape(4, 4))
+print("x =", x.reshape(8, 8))
+print("y =", y.reshape(8, 8))
 
 
 def circular_conv(x, y):
-    return np.fft.ifft(np.fft.fft(x) * np.fft.fft(y))
+    return np.real(np.fft.ifft(np.fft.fft(x) * np.fft.fft(y)))
 
 
 for k in range(len(x)):
-    rho, _, _, _, probs = qbc_conv(x, y, k, 6)
+    rho, _, _, _, probs = qbc_conv(x, y, k, 8)
 
     def convolution_analytic(x, y, k):
         sum = 0
---
+++ qbc_ipe.py
@@ -0,0 +1,121 @@
+import pennylane as qml
+from pennylane import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+
+def qbc_ipe_algorithm(x, y, num_t_wires=10):
+    assert len(x) == len(y)
+    num_n_wires = int(np.ceil(np.log2(len(x))))
+    num_tot_wires = num_t_wires + num_n_wires + 1
+
+    t_wires = range(0, num_t_wires)
+    n_wires = range(num_t_wires, num_t_wires + num_n_wires)
+    a_wires = range(num_t_wires + num_n_wires, num_t_wires + num_n_wires + 1)
+    tot_wires = range(0, num_t_wires + num_n_wires + 1)
+
+    dev = qml.device("default.qubit", wires=tot_wires, shots=10000)
+
+    A_x = np.zeros((2**num_n_wires, 2**num_n_wires))
+    A_x[:, 0] = x
+    Q_x, _ = la.qr(A_x, mode="full")
+
+    u_x = Q_x.T
+    if np.sign(u_x[0, 0]) != np.sign(x[0]):
+        u_x *= -1
+
+    # Oracle A encoding x data
+    def U_x():
+        qml.QubitUnitary(u_x, wires=n_wires)
+
+    A_y = np.zeros((2**num_n_wires, 2**num_n_wires))
+    A_y[:, 0] = y
+    Q_y, _ = la.qr(A_y, mode="full")
+
+    u_y = Q_y.T
+    if np.sign(u_y[0, 0]) != np.sign(y[0]):
+        u_y *= -1
+
+    # Oracle B encoding y data
+    def U_y():
+        qml.QubitUnitary(u_y, wires=n_wires)
+
+    x_minus_y = np.array(x) - np.array(y)
+    A_x_minus_y = np.zeros((2**num_n_wires, 2**num_n_wires))
+    A_x_minus_y[:, 0] = x_minus_y
+    Q_x_minus_y, _ = la.qr(A_x_minus_y, mode="full")
+    u_x_minus_y = Q_x_minus_y.T
+    if np.sign(u_x_minus_y[0, 0]) != np.sign(x_minus_y[0]):
+        u_x_minus_y *= -1
+
+    # Oracle B encoding y data
+    def U_x_minus_y():
+        qml.QubitUnitary(u_x_minus_y, wires=n_wires)
+
+    def U_psi_good():
+        qml.PauliX(wires=a_wires)
+        U_x_minus_y()
+
+    # Phase oracle used in Grover operator
+    def A_operator(U_x, U_y):
+        qml.Hadamard(wires=a_wires)
+        qml.ctrl(U_x, control=a_wires, control_values=[0])()
+        qml.PauliX(wires=a_wires)
+        qml.ctrl(U_y, control=a_wires, control_values=[0])()
+        qml.PauliX(wires=a_wires)
+        qml.Hadamard(wires=a_wires)
+
+    # Grover operator used in QPE
+    def grover_operator():
+        qml.adjoint(U_psi_good)()
+        for i in n_wires:
+            qml.PauliX(wires=i)
+        qml.PauliX(wires=a_wires)
+        qml.ctrl(
+            qml.PauliZ,
+            control=n_wires,
+            control_values=np.ones(num_n_wires),
+        )(wires=a_wires)
+        for i in n_wires:
+            qml.PauliX(wires=i)
+        qml.PauliX(wires=a_wires)
+        U_psi_good()
+
+        A_operator(U_x, U_y)
+
+        for i in n_wires:
+            qml.PauliX(wires=i)
+        qml.PauliX(wires=a_wires)
+        qml.ctrl(
+            qml.PauliZ,
+            control=n_wires,
+            control_values=np.ones(num_n_wires),
+        )(wires=a_wires)
+        for i in n_wires:
+            qml.PauliX(wires=i)
+        qml.PauliX(wires=a_wires)
+
+        qml.adjoint(A_operator)(U_x, U_y)
+
+    # QBC circuit
+    @qml.qnode(dev, interface=None)
+    def qbc(U_x, U_y):
+        A_operator(U_x, U_y)
+
+        for t in t_wires:
+            qml.Hadamard(wires=t)
+
+        for idx, t in enumerate(t_wires):
+            for i in range(2 ** (num_t_wires - idx - 1)):
+                qml.ctrl(grover_operator, control=t)()
+
+        qml.adjoint(qml.QFT(wires=t_wires))
+
+        return qml.probs(wires=t_wires), qml.counts(wires=t_wires)
+
+    probs, counts = qbc(U_x, U_y)
+    j = np.argmax(probs)
+    theta = 2 * np.pi * j / 2**num_t_wires
+    f = np.sin(theta / 2) ** 2
+    rho = -(1.0 - 2.0 * f) * la.norm(x) * la.norm(y)
+    return rho, f, theta, j, probs, counts
